c27c1f48602fdbc1925022a1d935f230ba36ef51
Chris <chrischong030@gmail.com>
2020-07-22 17:49:43 +1000

    Add background image to UIImageView in ViewController 1 & 2

---
+++ ADS102-Fruit-Salad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADS102-Fruit-Salad
+//
+//  Created by wade chen on 22/7/20.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
